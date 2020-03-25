0977a8a0b5da742c59d9a4f6debc8143f9f8f6e6
lucky1523 <50339785+lucky1523@users.noreply.github.com>
2020-03-24 21:52:52 -0400

    Update Consulta 1.sql

--- ProyectoFinalInventario/Consulta 1.sql
+++ ProyectoFinalInventario/Consulta 1.sql
@@ -2,4 +2,4 @@ use inventario;
 select nombres, direccion, ID
 from empleado
 inner join persona on empleado.personaID = persona.CI and sueldo >= 2200;
-select avg(sueldo) from empleado where sueldo = 2200;
+select avg(sueldo) from empleado where sueldo >= 2200;
